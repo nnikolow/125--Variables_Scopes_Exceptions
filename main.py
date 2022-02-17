3dde9881a76dce9754f043db67a4b8db684b82b9
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-17 14:55:26 +0200

    f1

--- main.py
+++ main.py
@@ -2,25 +2,13 @@ import os
 
 # opening the file
 try:
-    my_file = open("my_file.txt", "r")
+    file_location = input("Whats the location of the file?\n")
+    my_file = open(file_location, "r")
 
 except IOError:
     print("The given file name / path is incorrect.")
 finally:
     pass
-
-# asking the user if he wants to know the file path
-choice = True
-while choice:
-    x = input("Do you want to know the location of the file? \n y/n \n")
-    if x == "y":
-        print("The path of the file is: ", os.path.abspath("my_file.txt"))
-        choice = False
-    elif x == "n":
-        print("Ok. Moving forward...")
-        choice = False
-    else:
-        pass
 
 # showing the contents of the file
 print("Contents of the file:\n", my_file.read())
@@ -28,12 +16,14 @@ file1 = open("my_file.txt").read().splitlines()
 
 
 # creating the second file and converting F to C
-file2 = open("my_file2.txt", "w")
-
-for line in file1:
-    if "F" in line:
-        file2.write(line + '\n')
-    else:
-        file2.write(str(((float(line[:-1]) * 9) / 5) + 32) + 'F' + '\n')
-file2.close()
+file2 = open("my_file2.txt", "w", encoding='utf8')
+try:
+    for line in file1:
+        if "F" in line:
+            file2.write(line + '\n')
+        else:
+            file2.write(str(((float(line[:-1]) * 9) / 5) + 32) + 'F' + '\n')
+    file2.close()
+except ValueError:
+    print("Incorrect format of the file.")
 
